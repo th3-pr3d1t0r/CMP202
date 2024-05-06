45df6b93e9a8ed04ca5c7c6e5137955e446d24d2
pr3d1t0r <ugochukwuanosike1@gmail.com>
2024-05-06 09:07:05 +0100

    first commit

---
+++ src/Cmp_202.java
@@ -0,0 +1,18 @@
+public class Cmp_202 {
+        int age;
+        float height;
+        String name;
+        String password;
+
+        void printUserDetails() {
+            age = 23;
+            height = 172;
+            name = "Dan-Codes";
+            name.length();
+            password = "User@123";
+            if (password.contains("123") || password.contains("User")) {
+                System.out.println("Password is not strong enough");
+            }
+        }
+    }
+
